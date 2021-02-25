b88a2f6a23e65c37a9b1fba07b8f608a2fc38518
Sasan R.Nami <nami.srn@gmail.com>
2021-02-25 16:14:21 +0100

    test2

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+   
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+                
     }
 }
